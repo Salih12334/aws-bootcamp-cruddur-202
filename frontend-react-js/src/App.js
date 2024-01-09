050c278f9b5297a2ca86dd163ef0d7c8d804681f
Salih12334 Yılboğa <salihyilboga98@gmail.com>
2024-01-09 15:53:47 +0000

    changed

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import { Amplify } from "aws-amplify";
 import HomeFeedPage from './pages/HomeFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import NotificationFeedPage from "./pages/NotifcationFeedPage"
@@ -15,7 +15,19 @@ import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
-
+Amplify.configure({
+  "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
+  "aws_cognito_identity_pool_id": process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
+  "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
+  "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POLLS_ID,
+  "aws_user_pools_web_client_id": process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
+  oauth: {},
+  Auth: {
+    region: process.env.REACT_APP_AWS_PROJECT_REGION,
+    userPoolId:process.env.REACT_APP_AWS_USER_POLLS_ID,
+    userPoolWebClientId:process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID
+  }
+});
 const router = createBrowserRouter([
   {
     path: "/",
